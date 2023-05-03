b01f020debcd838fafdd94afc0fcb2be46d421b6
Jeremie4 <126706125+Jeremie4@users.noreply.github.com>
2023-05-03 11:07:26 -0600

    Updated to delete old list.

--- List Assignment.py
+++ List Assignment.py
@@ -1,27 +1,39 @@
-import tkinter
+# Import modules
+import list_assignment_lib
 import customtkinter
+import tkinter
 import sys
-import webbrowser
 
-customtkinter.set_appearance_mode("Dark")  # Modes: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
-customtkinter.set_widget_scaling(1)  # widget dimensions and text size
-customtkinter.set_window_scaling(1)  # window geometry dimensions
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
+# Set theme
+customtkinter.set_appearance_mode("Dark")
+customtkinter.set_default_color_theme("blue")
+customtkinter.set_widget_scaling(1)
+customtkinter.set_window_scaling(1)
+
+
+# Setup window
+app = customtkinter.CTk()
 app.geometry("1200, 900")
-app.minsize(1200, 900)
+app.minsize(1024, 768)
 app.maxsize(2160, 3840)
 app.title("List Assignment - Jeremie Marquis")
 
-shopping_list = []
-x = 0
+
+# Define variables
+Electronics = []
+Groceries = []
+Clothes = []
+Books = []
 time_ran = 0
+x = 0
 
 
-for i in range(6):
-    x += 0.1
+# Make checkboxes
+for i in range(4):
+    x += 0.15
     y = 0.05
+
     for i in range(1,17):
         if time_ran == 0:
             num = i
@@ -31,52 +43,74 @@ for i in range(6):
             num = i + 32
         elif time_ran == 3:
             num = i + 48
-        elif time_ran == 4:
-            num = i + 64
-        elif time_ran == 5:
-            num = i + 80
+
         y += 0.05
         y = round(y, 2)
         command = ('checkboxtext' + str(num) + ' = ' +  '"item ' + str(num) + '"\ncheckbox' + str(num) + ' = customtkinter.CTkCheckBox(master=app, text=checkboxtext' + str(num) + ')\ncheckbox' + str(num) + '.place(relx=' + str(x) + ', rely=' + str(y) + ', anchor=tkinter.W)')
         exec(command)
+
     time_ran += 1
 
 
-def cancel_order():
-    textbox = customtkinter.CTkTextbox(master=app)
-    textbox.insert("0.0", "Order Canceled")
+# Delete list
+def delete_list():
+    Groceries.clear()
+    Clothes.clear()
+    Electronics.clear()
+    Books.clear()
+    textbox = customtkinter.CTkTextbox(master=app, height=400)
+    textbox.insert("0.0", "List deleted")
     textbox.configure(state="disabled")
-    textbox.place(relx=0.8, rely=0.3, anchor=tkinter.CENTER)
-    button = customtkinter.CTkButton(master=app, text="Place Order", command=place_order)
+    textbox.place(relx=0.8, rely=0.45, anchor=tkinter.CENTER)
+    button = customtkinter.CTkButton(master=app, text="Make List", command=make_list)
     button.place(relx=0.8, rely=0.1, anchor=tkinter.CENTER)
 
 
-def place_order():
-    for i in range(1,97):
+# Make list
+def make_list():
+    for i in range(1,65):
         num = i
-        command = ('if checkbox' + str(num) + '.get() == 1:\n    shopping_list.append(checkboxtext' + str(num) + ')')
+        if num <= 16:
+            command = ('if checkbox' + str(num) + '.get() == 1:\n    Groceries.append(checkboxtext' + str(num) + ')')
+        elif num <= 32:
+            command = ('if checkbox' + str(num) + '.get() == 1:\n    Clothes.append(checkboxtext' + str(num) + ')')
+        elif num <= 48:
+            command = ('if checkbox' + str(num) + '.get() == 1:\n    Electronics.append(checkboxtext' + str(num) + ')')
+        elif num <= 64:
+            command = ('if checkbox' + str(num) + '.get() == 1:\n    Books.append(checkboxtext' + str(num) + ')')
+
         exec(command)
 
-    
-    textbox = customtkinter.CTkTextbox(master=app)
-    textbox.delete("0.0", "end")
-    textbox.insert("0.0", shopping_list)
+    textbox = customtkinter.CTkTextbox(master=app, height=400)
+    textbox.insert("0.0", "Groceries: " + str(Groceries) + "\n")
+    textbox.insert("end", "Clothes: " + str(Clothes) + "\n")
+    textbox.insert("end", "Electronics: " + str(Electronics) + "\n")
+    textbox.insert("end", "Books: " + str(Books) + "\n")
     textbox.configure(state="disabled")
-    textbox.place(relx=0.8, rely=0.3, anchor=tkinter.CENTER)
-    #label = customtkinter.CTkLabel(master=app, text=(shopping_list), justify=customtkinter.LEFT)
-    #label.place(relx=0.8, rely=0.2, anchor=tkinter.CENTER)
-    button = customtkinter.CTkButton(master=app, text="Cancel Order", command=cancel_order)
+    textbox.place(relx=0.8, rely=0.45, anchor=tkinter.CENTER)
+    button = customtkinter.CTkButton(master=app, text="Delete List", command=delete_list)
     button.place(relx=0.8, rely=0.1, anchor=tkinter.CENTER)
 
-def open_link():
-    webbrowser.open('https://github.com/Jeremie4/Menu-Project')
 
-button = customtkinter.CTkButton(master=app, text="Place Order", command=place_order)
+# Make labels
+label_1 = customtkinter.CTkLabel(master=app, text="Groceries", justify=customtkinter.LEFT)
+label_1.place(relx=0.2, rely=0.05, anchor=tkinter.CENTER)
+label_2 = customtkinter.CTkLabel(master=app, text="Clothes", justify=customtkinter.LEFT)
+label_2.place(relx=0.35, rely=0.05, anchor=tkinter.CENTER)
+label_3 = customtkinter.CTkLabel(master=app, text="Electronics", justify=customtkinter.LEFT)
+label_3.place(relx=0.5, rely=0.05, anchor=tkinter.CENTER)
+label_4 = customtkinter.CTkLabel(master=app, text="Books", justify=customtkinter.LEFT)
+label_4.place(relx=0.65, rely=0.05, anchor=tkinter.CENTER)
+
+
+# Make buttons
+button = customtkinter.CTkButton(master=app, text="Make List", command=make_list)
 button.place(relx=0.8, rely=0.1, anchor=tkinter.CENTER)
-exit_button = customtkinter.CTkButton(master=app, text="Exit", command=(sys.exit))
+exit_button = customtkinter.CTkButton(master=app, text="Exit", command=sys.exit)
 exit_button.place(relx=0.8, rely=0.85, anchor=tkinter.CENTER)
-github_button = customtkinter.CTkButton(master=app, text="GitHub", command=open_link)
+github_button = customtkinter.CTkButton(master=app, text="GitHub", command=list_assignment_lib.open_GitHub_link)
 github_button.place(relx=0.8, rely=0.8, anchor=tkinter.CENTER)
 
 
+# Run app
 app.mainloop()
---
+++ list_assignment_lib.py
@@ -0,0 +1,5 @@
+import webbrowser
+
+# Open GitHub link
+def open_GitHub_link():
+    webbrowser.open('https://github.com/Jeremie4/List-Assignment')
